ebebdfe6aa247b98934de2eb1168427b326c99ba
Primoz Cigler <primoz.cigler@gmail.com>
2014-10-06 22:17:03 +0200

    Added the GA tracking code for the Carpress - experimental.

--- index.php
+++ index.php
@@ -205,5 +205,18 @@ function site_url( $uri = "" ) {
 			</div>
 		</div>
 		<iframe src="<?php echo $item['demo_url']; ?>" frameborder="0" id="main-preview-frame"></iframe>
+		<?php if ( 'carpress-wp' === $_GET['theme'] ) : ?>
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-33538073-10', 'auto', {'allowLinker': true});
+		ga('require', 'linker');
+		ga('linker:autoLink', ['carpress.demo.proteusthemes.com'] );
+		ga('send', 'pageview');
+		</script>
+		<?php endif; ?>
 	</body>
 </html>
