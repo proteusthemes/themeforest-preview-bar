bd584daf26290db64d9592bdafbe19fe831c71a2
Primoz Cigler <primoz@proteusnet.com>
2015-03-13 17:52:38 +0100

    Added FB tracking pixel.

--- index.php
+++ index.php
@@ -32,6 +32,24 @@ require_once 'preview-bar/items.php';
 		window.addEventListener( 'resize', calcHeight );
 	</script>
 
+	<!-- fb tracking pixel -->
+	<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+  _fbq.push(['addPixelId', '830709590327754']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=830709590327754&amp;ev=PixelInitialized" /></noscript>
+
 	</head>
 
 	<body>
