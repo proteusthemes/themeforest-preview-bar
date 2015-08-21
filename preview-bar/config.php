e47f8a3c390a750feea8d799dd05f722f8fa0e07
Primoz Cigler <primoz@proteusnet.com>
2015-08-21 12:36:28 +0300

    Updated prices and added LegalPress.

--- preview-bar/config.php
+++ preview-bar/config.php
@@ -30,14 +30,14 @@ $items = array(
 		// needs any explanation? URL to your item in ThemeForest
 		'url'         => 'http://themeforest.net/item/hairpress-html-template-for-hair-salons/3803346?license=regular&open_purchase_for_item_id=3803346&purchasable=source',
 		// Price of the item
-		'price'       => '$17',
+		'price'       => '$18',
 	),
 	'hairpress-wp' => array(
 		'title'       => 'Hairpress - Wordpress Theme for Hair Salons Preview - by ' . ENVATO_USERNAME,
 		'title_short' => 'Hairpress WP',
 		'demo_url'    => '//demo.proteusthemes.com/hairpress/',
 		'url'         => 'http://themeforest.net/item/hairpress-wordpress-theme-for-hair-salons/4099496?license=regular&open_purchase_for_item_id=4099496&purchasable=source',
-		'price'       => '$58',
+		'price'       => '$59',
 		'analytics'   => array(
 			'tracking_id'     => 'UA-33538073-4',
 			'allowed_domains' => array(
@@ -50,35 +50,35 @@ $items = array(
 		'title_short' => 'Webmarket HTML',
 		'demo_url'    => '//www.proteusthemes.com/themes/webmarket-html/',
 		'url'         => 'http://themeforest.net/item/webmarket-html-template-for-online-shop/5409539?license=regular&open_purchase_for_item_id=5409539&purchasable=source',
-		'price'       => '$16',
+		'price'       => '$17',
 	),
 	'webmarket-wp' => array(
 		'title'       => 'Webmarket - WP WooCommerce Theme for Online Shop Preview - by ' . ENVATO_USERNAME,
 		'title_short' => 'Webmarket WP + Woo',
 		'demo_url'    => '//demo.proteusthemes.com/webmarket/',
 		'url'         => 'http://themeforest.net/item/webmarket-wp-woocommerce-theme-for-online-shop/6437728?license=regular&open_purchase_for_item_id=6437728&purchasable=source',
-		'price'       => '$58',
+		'price'       => '$59',
 	),
 	'organique-html' => array(
 		'title'       => 'Organique - HTML Template For Healthy Food Store Preview - by ' . ENVATO_USERNAME,
 		'title_short' => 'Organique HTML',
 		'demo_url'    => '//www.proteusthemes.com/themes/organique-html/',
 		'url'         => 'http://themeforest.net/item/organique-html-template-for-healthy-food-store/6779086?license=regular&open_purchase_for_item_id=6779086&purchasable=source',
-		'price'       => '$17',
+		'price'       => '$18',
 	),
 	'organique-wp' => array(
 		'title'       => 'Organique - WordPress Theme For Healthy Food Shop Preview - by ' . ENVATO_USERNAME,
 		'title_short' => 'Organique WP + Woo',
 		'demo_url'    => '//demo.proteusthemes.com/organique/',
 		'url'         => 'http://themeforest.net/item/organique-wordpress-theme-for-healthy-food-shop/7312458?license=regular&open_purchase_for_item_id=7312458&purchasable=source',
-		'price'       => '$58',
+		'price'       => '$59',
 	),
 	'carpress-wp' => array(
 		'title'       => 'Carpress - WordPress Theme For Mechanic Workshops Preview - by ' . ENVATO_USERNAME,
 		'title_short' => 'Carpress WP',
 		'demo_url'    => '//demo.proteusthemes.com/carpress/',
 		'url'         => 'http://themeforest.net/item/carpress-wordpress-theme-for-mechanic-workshops/7042577?license=regular&open_purchase_for_item_id=7042577&purchasable=source',
-		'price'       => '$58',
+		'price'       => '$59',
 		'analytics'   => array(
 			'tracking_id'     => 'UA-33538073-10',
 			'allowed_domains' => array(
@@ -91,21 +91,21 @@ $items = array(
 		'title_short' => 'Readable HTML',
 		'demo_url'    => '//www.proteusthemes.com/themes/readable-html/',
 		'url'         => 'http://themeforest.net/item/readable-blog-template-focused-on-readability/7499064?license=regular&open_purchase_for_item_id=7499064&purchasable=source',
-		'price'       => '$16',
+		'price'       => '$17',
 	),
 	'readable-wp' => array(
 		'title'       => 'Readable - WordPress Theme Focused on Readability Preview - by ' . ENVATO_USERNAME,
 		'title_short' => 'Readable WP',
 		'demo_url'    => '//demo.proteusthemes.com/readable/',
 		'url'         => 'http://themeforest.net/item/readable-wordpress-theme-focused-on-readability/7712790?license=regular&open_purchase_for_item_id=7712790&purchasable=source',
-		'price'       => '$43',
+		'price'       => '$44',
 	),
 	'restaurant-wp' => array(
 		'title'       => 'Dining Restaurant - WordPress Theme For Chefs - by ' . ENVATO_USERNAME,
 		'title_short' => 'Dining Restaurant WP',
 		'demo_url'    => '//demo.proteusthemes.com/restaurant/',
 		'url'         => 'http://themeforest.net/item/dining-restaurant-wordpress-theme-for-chefs/8294419?license=regular&open_purchase_for_item_id=8294419&purchasable=source',
-		'price'       => '$58',
+		'price'       => '$59',
 		'analytics'   => array(
 			'tracking_id'     => 'UA-33538073-11',
 			'allowed_domains' => array(
@@ -118,7 +118,7 @@ $items = array(
 		'title_short' => 'BuildPress WP',
 		'demo_url'    => '//demo.proteusthemes.com/buildpress/',
 		'url'         => 'http://themeforest.net/item/buildpress-wp-theme-for-construction-business/9323981?license=regular&open_purchase_for_item_id=9323981&purchasable=source',
-		'price'       => '$58',
+		'price'       => '$59',
 		'analytics'   => array(
 			'tracking_id'     => 'UA-33538073-13',
 			'allowed_domains' => array(
@@ -131,7 +131,7 @@ $items = array(
 		'title_short' => 'MentalPress WP',
 		'demo_url'    => '//demo.proteusthemes.com/mentalpress/',
 		'url'         => 'http://themeforest.net/item/mentalpress-psychiatrists-wp-theme/10676732?license=regular&open_purchase_for_item_id=10676732&purchasable=source',
-		'price'       => '$58',
+		'price'       => '$59',
 		'analytics'   => array(
 			'tracking_id'     => 'UA-33538073-16',
 			'allowed_domains' => array(
@@ -144,7 +144,7 @@ $items = array(
 		'title_short' => 'CargoPress WP',
 		'demo_url'    => '//demo.proteusthemes.com/cargopress/',
 		'url'         => 'http://themeforest.net/item/cargopress-logistic-warehouse-transport-wp/11601531?license=regular&open_purchase_for_item_id=11601531&purchasable=source',
-		'price'       => '$58',
+		'price'       => '$59',
 		'analytics'   => array(
 			'tracking_id'     => 'UA-33538073-18',
 			'allowed_domains' => array(
@@ -152,19 +152,19 @@ $items = array(
 			),
 		),
 	),
-	// 'legalpress-wp' => array(
-	// 	'title'       => 'LegalPress - Law, Lawyer, Attorney WP Theme - by ' . ENVATO_USERNAME,
-	// 	'title_short' => 'LegalPress WP',
-	// 	'demo_url'    => '//demo.proteusthemes.com/legalpress/',
-	// 	'url'         => 'http://themeforest.net/item/cargopress-logistic-warehouse-transport-wp/11601531?license=regular&open_purchase_for_item_id=11601531&purchasable=source',
-	// 	'price'       => '$58',
-	// 	'analytics'   => array(
-	// 		'tracking_id'     => 'UA-33538073-18',
-	// 		'allowed_domains' => array(
-	// 			'demo.proteusthemes.com'
-	// 		),
-	// 	),
-	// ),
+	'legalpress-wp' => array(
+		'title'       => 'LegalPress - Law, Lawyer, Attorney WP Theme - by ' . ENVATO_USERNAME,
+		'title_short' => 'LegalPress WP',
+		'demo_url'    => '//demo.proteusthemes.com/legalpress/',
+		'url'         => 'http://themeforest.net/item/legalpress-law-attorney-insurance-legal-theme/12389860?license=regular&open_purchase_for_item_id=12389860&purchasable=source',
+		'price'       => '$60',
+		'analytics'   => array(
+			'tracking_id'     => 'UA-33538073-20',
+			'allowed_domains' => array(
+				'demo.proteusthemes.com'
+			),
+		),
+	),
 );
 
 // fix: first element last in the dropdown
